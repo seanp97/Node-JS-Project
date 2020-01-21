a9c77347a38abed4aff9dc941fcf0c4b4ed4df8c
seanp97 <34320661+seanp97@users.noreply.github.com>
2020-01-21 11:20:52 +0000

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ con.connect(function (err) {
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.urlencoded());
 
 app.get("/", function(req, res) {
     con.query("SELECT * FROM NodeSQL", function (err, result, fields) {
@@ -28,8 +29,12 @@ app.get("/", function(req, res) {
     //res.render("index", {layout: "main"});
 });
 
-app.get("about", function(req, res) {
-    res.render("about", {layout: "main"});
+app.get("/about", function(req, res) {
+    con.query("SELECT name FROM NodeSQL", function (err, result) {
+        if (err) throw err;
+        res.render("about", {layout: "main", data: result})
+    });
+    //res.render("about", {layout: "main"});
 });
 
-app.listen(8090);
+app.listen(8092);
